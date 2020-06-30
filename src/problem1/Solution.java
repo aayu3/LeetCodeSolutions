0596bf5a033cee798c109357c357ffaf5ac30b5f
Aaron Yu <aaronyu3@illinois.edu>
2020-06-30 14:25:25 -0700

    First Commit + Problem 1 Solution

---
+++ src/problem1/Solution.java
@@ -0,0 +1,27 @@
+package problem1;
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args){
+        int[] example = {2,7,11,15};
+        System.out.println(Arrays.toString(twoSum(example,9)));
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hmap = new HashMap<Integer, Integer>();
+        int[] retval = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int cur = nums[i];
+            if (hmap.get(target-cur) != null){
+                retval[0] =i;
+
+                retval[1] = hmap.get(target-cur);
+                return retval;
+            }
+            else {
+                hmap.put(cur,i);
+            }
+
+        }
+        return retval;
+    }
+}
