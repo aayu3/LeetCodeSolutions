25918edf4348d32d3204d3d2f5057a8c6e853f24
Aaron Yu <aaronyu3@illinois.edu>
2020-07-05 16:21:06 -0700

    Problem 11 solution, more optimized solution.
    Signed-off-by: Aaron Yu <aaronyu3@illinois.edu>

--- src/problem11/Solution.java
+++ src/problem11/Solution.java
@@ -1,7 +1,6 @@
 package problem11;
 
-import java.lang.reflect.Array;
-import java.util.*;
+
 
 class Solution {
     public static void main(String[] args) {
@@ -9,27 +8,42 @@ class Solution {
     }
     public static int maxArea(int[] height) {
         //keys are the heights of bars, values are the distance
-        SortedMap<Double, Integer> heightsDistance = new TreeMap<Double, Integer>();
-        List<Integer> distancesLeft = new ArrayList<Integer>();
         int maxVol = 0;
-        for (int i = 1; i <= height.length; i++) {
-            distancesLeft.add(i);
-            //we add fractional parts so duplicate heights are counted as distinct, but when we do actual calculations, we round down
-            heightsDistance.put(height[i-1] + (double) (i-1)/(height.length),i);
-        }
-        for (int i = 0; i < height.length -1; i++) {
-            //the smaller height
-            double firstHeight = heightsDistance.firstKey();
-            int distanceFH = heightsDistance.get(firstHeight);
-
-            distancesLeft.remove(Integer.valueOf(heightsDistance.get(firstHeight)));
-            if (Math.floor(firstHeight)*Math.abs(distancesLeft.get(0)-heightsDistance.get(firstHeight)) >= Math.floor(firstHeight)*Math.abs(distancesLeft.get(distancesLeft.size()-1) - heightsDistance.get(firstHeight))) {
-                maxVol = Math.max(maxVol,(int) Math.floor(firstHeight)*Math.abs(distancesLeft.get(0)-heightsDistance.get(firstHeight)));
+        int leftPointer = 0;
+        int rightPointer = height.length-1;
+        while (leftPointer < rightPointer) {
+            int leftHeight = height[leftPointer];
+            int rightHeight = height[rightPointer];
+            if (leftHeight <= rightHeight) {
+                maxVol = Math.max(maxVol,leftHeight*(rightPointer-leftPointer));
+                //amount to increment left pointer by without being greater than curr leftpointer
+                //i.e. how many things we can skip while incrementing leftpointer to the right
+                int amtIncr = 1;
+                while (leftHeight >= height[leftPointer + amtIncr]) {
+                    if (leftPointer + amtIncr >= rightPointer) {
+                        //if this happens, everything to the right of left pointer is smaller than curr left pointer and
+                        //we are done
+                        return maxVol;
+                    }
+                    amtIncr += 1;
+                }
+                leftPointer += amtIncr;
             }
             else {
-                maxVol = Math.max(maxVol,(int) Math.floor(firstHeight) * Math.abs(distancesLeft.get(distancesLeft.size() - 1) - heightsDistance.get(firstHeight)));
+                maxVol = Math.max(maxVol,rightHeight*(rightPointer-leftPointer));
+                //amount we can decrement right pointer by without being greater than curr rightpointer
+                //i.e. how many things we can skip while decrementing rightpointer to the left
+                int amtDcr = 1;
+                while (rightHeight >= height[rightPointer - amtDcr]) {
+                    if (rightPointer - amtDcr <= leftPointer) {
+                        //if this happens, everything to the left of right pointer is smaller than curr right pointer and
+                        //we are done
+                        return maxVol;
+                    }
+                    amtDcr += 1;
+                }
+                rightPointer -= amtDcr;
             }
-            heightsDistance.remove(firstHeight);
         }
         return maxVol;
 
